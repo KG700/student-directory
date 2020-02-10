13c0b0f1aed37a6d7044723a3a2321d05fe9394e
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-10 15:24:06 +0000

    Only prints names that are shorter than 12 characters

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def print_header
 end
 def print(students)  
   students.each_with_index do |student, i|
-    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0] == "K"
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].size < 12
   end
 end
 def print_footer(names)
