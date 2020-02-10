134e83a2adc75754a1a2da5169ba3a83e77178f3
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-10 15:15:49 +0000

    Prints number before each name (8.1)

--- directory.rb
+++ directory.rb
@@ -1,25 +1,10 @@
-# Let's put all the students into an array
-#students = [
-#  {name: "Dr. Hannibal Lecter", cohort: :november},
-#  {name: "Darth Vader", cohort: :november},
-#  {name: "Nurse Ratched", cohort: :november},
-#  {name: "Michael Corleone", cohort: :november},
-#  {name: "Alex DeLarge", cohort: :november},
-#  {name: "The Wicked Witch of the West", cohort: :november},
-#  {name: "Terminator", cohort: :november},
-#  {name: "Freddy Krueger", cohort: :november},
-#  {name: "The Joker", cohort: :november},
-#  {name: "Joffrey Baratheon", cohort: :november},
-#  {name: "Norman Bates", cohort: :november}
-#  ]
-  
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 def print(students)  
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, i|
+    puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(names)
