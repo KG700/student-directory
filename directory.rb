58123435f5666f8035de95ad678e0c230eccdda0
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-10 15:22:33 +0000

    Only prints names starting with 'K'

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def print_header
 end
 def print(students)  
   students.each_with_index do |student, i|
-    puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0] == "K"
   end
 end
 def print_footer(names)
