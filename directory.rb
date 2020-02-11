1d0bc420377e613a7a556b75f46b1b5104cfa7b5
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-11 16:20:34 +0000

    Added interactive_menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,30 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. Print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    
+    # 2. Read the input and save it into a variable
+    selection = gets.chomp
+    
+    # 3. Do what the user has asked
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print_cohort(students)
+        print_footer(students)
+      when "9"
+        exit # this will cause the program to terminate
+      else
+        puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def print_header
   puts "The students at Makers Academy"
   puts "-------------"
@@ -89,9 +116,4 @@ def get_country_of_birth
   cob
 end
 
-#nothing happens until we've called the methods
-students = input_students
-print_header
-#print(students)
-print_cohort(students)
-print_footer(students)
+interactive_menu
