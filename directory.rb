1558e26fa546d8241593dec3f1f4f9eca1ac665f
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-11 13:21:49 +0000

    Prints students by cohort (8.8) and adjusts footer for 1 student

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 def print_header
-  puts "The students of my cohort at Makers Academy"
+  puts "The students at Makers Academy"
   puts "-------------"
 end
 def print(students)
@@ -9,44 +9,42 @@ def print(students)
     counter += 1
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+def print_cohort(students)
+  existing_cohorts = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December, :"Not specified"]
+  existing_cohorts.each do |cohort| 
+    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:cohort] == cohort }
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great student#{'s' if students.size > 1}"
 end
 
 def input_students
   puts "Please enter the names of the students"
-  puts "to edit any of your entries type 'edit'"
-  puts "to finish, just hit return twice"
-  #create an empty array
+  puts "to edit any of your entries type 'edit' and to finish, just hit return twice"
+
   students = []
-  #get the first name
+  #get the first name & cohort
   name = gets.chomp
-  puts "What cohort are they in?"
-  cohort = gets.chomp
-  cohort = 'Feb' if cohort.empty?
+  cohort = get_cohort
   
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym, hobbies: "", country_of_birth: "", height: ""} if name != 'edit' && cohort != 'edit'
+    students << {name: name, cohort: cohort} if name != 'edit'
     puts "Now we have #{students.count} students"
     print(students)
-    puts "Please enter the names of the students"
-    puts "to edit any of your entries type 'edit' and to finish, just hit return twice"
+    puts "Please enter the names of the students (or type 'edit' or hit return twice to finish)"
     name = gets.chomp
     if name == 'edit'
-      edit_input(students) 
+      edit_input(students)
     elsif !name.empty?
-      puts "What cohort are they in?"
-      cohort = gets.chomp
-      edit_input(students) if cohort == 'edit'
-      cohort = 'Feb' if cohort.empty?
+      cohort = get_cohort      
     end
   end
   students
-end
-
-def input_instructions
 end
 
 def edit_input(students)
@@ -57,9 +55,31 @@ def edit_input(students)
   puts "You have entered '#{students[number - 1][update.to_sym]}'. What would you like to change this to?"
   students[number - 1][update.to_sym] = gets.chomp
 end
-  
+
+def get_name
+end
+
+def get_cohort
+  existing_cohorts = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December, :"Not specified"]
+  while true
+    puts "Cohort: "
+    cohort_entered = gets.chomp
+    cohort_entered = cohort_entered.empty? ? 'Not specified'.to_sym : cohort_entered.to_sym
+    cohort_verified = existing_cohorts.select { |cohort| cohort == cohort_entered}.join.to_sym
+    break if !cohort_verified.empty?
+  end
+  cohort_verified
+end
+
+def get_height
+end
+
+def get country_of_birth
+end
+
 #nothing happens until we've called the methods
 students = input_students
 print_header
-print(students)
+#print(students)
+print_cohort(students)
 print_footer(students)
