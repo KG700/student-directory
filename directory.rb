45a3939e916a427c01c088c6e3936b0a33f58f49
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-12 14:23:00 +0000

    Added load_students method

--- directory.rb
+++ directory.rb
@@ -10,6 +10,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -21,6 +22,8 @@ def process(selection)
         show_students
       when "3"
         save_students
+      when "4"
+        load_students
       when "9"
         exit # this will cause the program to terminate
       else
@@ -130,6 +133,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym, height: 0, cob: "NA"}
   end
   file.close
 end
