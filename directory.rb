2693f9c2f8d8360cc7c1db69e07e2a438386ec03
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-12 11:55:11 +0000

    Refactored interactive_menu by separating into methods

--- directory.rb
+++ directory.rb
@@ -1,83 +1,86 @@
 def interactive_menu
-  students = []
+  @students = [] #an empty array accessible to all methods
   loop do
-    # 1. Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    
-    # 2. Read the input and save it into a variable
-    selection = gets.chomp
-    
-    # 3. Do what the user has asked
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def process(selection)
     case selection
       when "1"
-        students = input_students
+        input_students
       when "2"
-        print_header
-        print_cohort(students)
-        print_footer(students)
+        show_students
       when "9"
         exit # this will cause the program to terminate
       else
         puts "I don't know what you meant, try again"
     end
   end
+
+def show_students
+  print_header
+  print_students_list_by_cohort
+  print_footer
 end
 
 def print_header
   puts "The students at Makers Academy"
   puts "-------------"
 end
-def print(students)
+def print_students_list
   counter = 0
-  while students.length > counter
-    puts "#{counter + 1}.".center(4) + "#{students[counter][:name]}".center(10) + "#{students[counter][:cohort]} cohort ".center(20) + "height: #{students[counter][:height]}cm ".center(20) + "birth country: #{students[counter][:cob]}".center(25)
+  while @students.length > counter
+    puts "#{counter + 1}.".center(4) + "#{@students[counter][:name]}".center(10) + "#{@students[counter][:cohort]} cohort ".center(20) + "height: #{@students[counter][:height]}cm ".center(20) + "birth country: #{@students[counter][:cob]}".center(25)
     counter += 1
   end
 end
 
-def print_cohort(students)
+def print_students_list_by_cohort
   existing_cohorts = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December, :"Not specified"]
   existing_cohorts.each do |cohort| 
-    students.each { |student| puts "#{student[:name]}".center(10) + "#{student[:cohort]} cohort".center(20) + "height: #{student[:height]}cm".center(20) + "birth country: #{student[:cob]}".center(14) if student[:cohort] == cohort }
+    @students.each { |student| puts "#{student[:name]}".center(10) + "#{student[:cohort]} cohort".center(20) + "height: #{student[:height]}cm".center(20) + "birth country: #{student[:cob]}".center(14) if student[:cohort] == cohort }
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great student#{'s' if students.size > 1}"
+def print_footer
+  puts "Overall, we have #{@students.count} great student#{'s' if @students.size > 1}"
 end
 
 def input_students
-
-  students = []
 
   loop do
     name = get_name
     break if name.empty?
     
     if name == 'edit'
-      edit_input(students)
+      edit_input
     else
-      students << {name: name, cohort: get_cohort, height: get_height, cob: get_country_of_birth}
+      @students << {name: name, cohort: get_cohort, height: get_height, cob: get_country_of_birth}
     end
 
-    puts "Now we have #{students.size} student#{'s' if students.size > 1}"
-    print(students)
+    puts "Now we have #{@students.size} student#{'s' if @students.size > 1}"
+    print_students_list
 
   end
-  students
 end
   
 
 
-def edit_input(students)
+def edit_input
   puts "Enter the number of the student you would like to edit"
   number = gets.chomp.to_i
   puts "Would you like to update the name (type: 'name'), cohort (type: 'cohort'), height (type: 'height') or country of birth (type 'cob')?"
   update = gets.chomp
-  puts "You have entered '#{students[number - 1][update.to_sym]}'. What would you like to change this to?"
-  students[number - 1][update.to_sym] = gets.chomp
+  puts "You have entered '#{@students[number - 1][update.to_sym]}'. What would you like to change this to?"
+  @students[number - 1][update.to_sym] = gets.chomp
 end
 
 def get_name
